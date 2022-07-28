dd47221f5a111a622396ddd7fdc0000483b6380a
Miguel <maxim467@gmail.com>
2022-07-28 12:16:59 +0200

    Project update

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 return [
     'gateway_title' => 'PaymentNetwork',
     'method_description' => 'Pay securely via Credit / Debit Card with PaymentNetwork',
-    'default_merchant_id' => '100001',
-    'default_secret' => 'Circle4Take40Idea',
+    'default_merchant_id' => '155928',
+    'default_secret' => 'm3rch4nts1gn4tur3k3y',
 ];
